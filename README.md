49af960a45e45ef52213f3d37d0d15591ffdabb2
julien <julien.thomas84@gmail.com>
2021-10-07 18:19:30 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 ![](./img/couverture_poly.png)
 
-# LOG3430 TP 1 
+# LOG3430 TP 2
 - Auteurs : Nerese Carlens && Thomas Julien
 - le dépot Github dispo [ici](https://github.com/korrigans84/test-python)
 ## Partie 1
 
-L'objectif de ce TP est de recouvrir les critères de tests RACC & RICC pour les
+L'objectif de ce TP est de recouvrir les critères de tests RACC & RICC pour les 2 prédicats suivants : 
+
+![](./img/predicats.png)
+
+Dans un second temps, nous chercherons le jeu de tests qui satisfait le critère VNS pour le prédicat S2.
 
 ### Execution
 Pour lancer la partie code du TP, vous pouvez executer la commande 
@@ -13,6 +17,8 @@ Pour lancer la partie code du TP, vous pouvez executer la commande
 ./run.sh
 ```
 
-Cela produira en console le jeu de test pour le critère VNF du prédicat S2, et créera aussi un fichier parameters.json
+Cela produira en console le jeu de test pour le critère VNF du prédicat S2, et créera aussi un fichier **parameters.json**
 à la racine du projet, qui contient l'ensemble des jeux de tests satisfaisant les critères RACC ou RICC, pour chaque
 clause majeure des prédicats S1 et S2.
+
+![](./img/output_S2_VNF.png)
--- criteres.py
+++ criteres.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import json
 import sys
+
 global verbose
+
 
 def main():
     res = RACC_RICC_Criteres()
@@ -16,8 +18,9 @@ def main():
         pass
 
     # VNS pour S2
-    S2_VNS()
+    S2_VNS(verbose=verbose)
     return res
+
 
 ############################################
 ##          Fonctions à tester            ##
@@ -27,7 +30,7 @@ def S1(P, H, T1, T2, T3):
     return (P and ((H and T1) or T2)) or (H and T2 and not T3)
 
 
-def S2(P, T3, T2):
+def S2(P, T2, T3):
     return P or (not T3 and T2)
 
 
@@ -49,20 +52,61 @@ def RACC_RICC_Criteres() -> dict:
         }
     }
     # On test les critères RACC & RICC pour chaque clause majeure respective
-    for majorClause in ['P', 'H', 'T1', 'T2', 'T3']: # Pour le prédicat S1
-        RACCParameters = S1_RACCParameters(majorClause)
+    for majorClause in ['P', 'H', 'T1', 'T2', 'T3']:  # Pour le prédicat S1
+        RACCParameters = S1_RACCParameters(majorClause, verbose=verbose)
         res["S1"]["RACC"][majorClause] = RACCParameters
-        RICCParameters = S1_RICCParameters(majorClause)
+        RICCParameters = S1_RICCParameters(majorClause, verbose=verbose)
         res["S1"]["RICC"][majorClause] = RICCParameters
-        if majorClause in ['P', 'T3', 'T2']: # Pour le prédicat S2
-            RACCParameters = S2_RACCParameters(majorClause)
+        if majorClause in ['P', 'T3', 'T2']:  # Pour le prédicat S2
+            RACCParameters = S2_RACCParameters(majorClause, verbose=verbose)
             res["S2"]["RACC"][majorClause] = RACCParameters
-            RICCParameters = S2_RICCParameters(majorClause)
+            RICCParameters = S2_RICCParameters(majorClause, verbose=verbose)
             res["S2"]["RICC"][majorClause] = RICCParameters
     return res
 
 
-def S1_RACCParameters(majorClause: str):
+def S1_tests():
+    """
+    Cette fonction défini chaque tests possibles pour S1
+    :return: Générateur contenant les valeur des paramètres pour chaque test possible
+    L'utilisation d'un générateur permet d'optimiser le code
+    """
+    return ([P, H, T1, T2, T3] for P in [True, False] for H in [True, False] for T1 in [True, False] for T2 in
+            [True, False] for T3 in [True, False])
+
+def S2_tests():
+    """
+    Cette fonction défini chaque tests possibles pour S2
+    :return: Générateur contenant les valeur des paramètres pour chaque test possible
+    L'utilisation d'un générateur permet d'optimiser le code
+    """
+    return ([P, T2, T3] for P in [True, False] for T2 in [True, False] for T3 in [True, False])
+
+
+def S1_clause_to_index(clause: str):
+    """
+    Cette fonction permet de retourner l'index correspondant à la clause. Par exemple, H est à l'index 1, T3 à l'index 4
+    :param clause: La clause pour lequel on veut retrouver l'index
+    :return: l'index
+    """
+    S1_clauses = ['P', 'H', 'T1', 'T2', 'T3']
+    assert clause in S1_clauses
+
+    return S1_clauses.index(clause)
+
+def S2_clause_to_index(clause: str):
+    """
+    Cette fonction permet de retourner l'index correspondant à la clause.
+    :param clause: La clause pour lequel on veut retrouver l'index
+    :return: l'index
+    """
+    S2_clauses = ['P', 'T2', 'T3']
+    assert clause in S2_clauses
+
+    return S2_clauses.index(clause)
+
+
+def S1_RACCParameters(majorClause: str, verbose=False):
     """
     Cette fonction test chaque combinaisons de paramètres d'entrée et test si le jeu de paramètre défini un
     jeu de test pour le critère RACC, i.e le changent de la clause majeure entraine un changement du prédicat,
@@ -73,30 +117,24 @@ def S1_RACCParameters(majorClause: str):
     assert majorClause in ['P', 'H', 'T1', 'T2', 'T3']
     if verbose:
         print(f"Test du critère RACC pour le prédicat S1, avec la clause majeure {majorClause}")
-    jeux_de_parametres = []
-    for P in [True, False]:
-        for H in [True, False]:
-            for T1 in [True, False]:
-                for T2 in [True, False]:
-                    for T3 in [True, False]:
-                        if T1 is True and T3 is True:
-                            # T1 et T3 ne peuvent être vrai en même temps
-                            pass
-                        else:
-                            dict = {"P": P, "H": H, "T1": T1, "T2": T2, "T3": T3}
-                            dict[majorClause] = True  # On test si la clause majeure est vraie
-                            test1 = S1(dict['P'], dict['H'], dict['T1'], dict['T2'], dict['T3'])
-                            dict[majorClause] = False  # On test si la clause majeure est fausse
-                            test2 = S1(dict['P'], dict['H'], dict['T1'], dict['T2'], dict['T3'])
-                            if not test1 == test2:  # On vérifie que le prédicat a été une fois vrai et une fois faux
-                                dict.pop(majorClause)  # On enlève la clause majeure du jeu de paramètres
-                                jeux_de_parametres.append(dict)
+    parameters_sets = []
+    index = S1_clause_to_index(majorClause)
+    for test in S1_tests():
+        test[index] = True
+        test1 = S1(*test)
+        test[index] = False
+        test2 = S1(*test)
+        if test1 != test2:  # On vérifie que le prédicat a été une fois vrai et une fois faux
+            test.pop(index)  # On retire la clause majeure du jeu de paramètre
+            if test not in parameters_sets:  # On vérifie également que le test n'a pas été déjà effectué, car sinon on aurait chaque test en double.
+                parameters_sets.append(test)
     if verbose:
-        percentage_of_matched_tests = round(len(jeux_de_parametres) / (2 ** 6) * 100, 2)
+        percentage_of_matched_tests = round(len(parameters_sets) / (2 ** 6) * 100, 2)
         print(f'{percentage_of_matched_tests} % des tests fonctionnent')
-    return jeux_de_parametres
+    return parameters_sets
 
-def S1_RICCParameters(majorClause: str):
+
+def S1_RICCParameters(majorClause: str, verbose=False):
     """
     Cette fonction test chaque combinaisons de paramètres d'entrée et test si le jeu de paramètre défini un
     jeu de test pour le critère RICC, i.e le changent de la clause majeure n'entraine pas un changement du prédicat,
@@ -106,30 +144,24 @@ def S1_RICCParameters(majorClause: str):
     """
     if verbose:
         print(f"Test du critère RICC pour le prédicat S1, avec la clause majeure {majorClause}")
-    jeux_de_parametres = []
-    for P in [True, False]:
-        for H in [True, False]:
-            for T1 in [True, False]:
-                for T2 in [True, False]:
-                    for T3 in [True, False]:
-                        if T1 is True and T3 is True:
-                            # T1 et T3 ne peuvent être vrai en même temps
-                            pass
-                        else:
-                            dict = {"P": P, "H": H, "T1": T1, "T2": T2, "T3": T3}
-                            dict[majorClause] = True  # On test si la clause majeure est vraie
-                            test1 = S1(dict['P'], dict['H'], dict['T1'], dict['T2'], dict['T3'])
-                            dict[majorClause] = False  # On test si la clause majeure est fausse
-                            test2 = S1(dict['P'], dict['H'], dict['T1'], dict['T2'], dict['T3'])
-                            if test1 == test2: # On vérifie que le prédicat ne dépende pas de la clause majeure
-                                dict.pop(majorClause) # on l'enlève du jeu de paramètre
-                                jeux_de_parametres.append(dict)
+    parameters_sets = []
+    index = S1_clause_to_index(majorClause)
+    for test in S1_tests():
+        test[index] = True
+        test1 = S1(*test)
+        test[index] = False
+        test2 = S1(*test)
+        if test1 == test2:  # On vérifie que le prédicat n'ai pas changé
+            test.pop(index)  # On retire la clause majeure du jeu de paramètre
+            if test not in parameters_sets:  # On vérifie également que le test n'a pas été déjà effectué, car sinon on aurait chaque test en double.
+                parameters_sets.append(test)
     if verbose:
-        percentage_of_matched_tests = round(len(jeux_de_parametres) / (2 ** 6) * 100, 2)
+        percentage_of_matched_tests = round(len(parameters_sets) / (2 ** 6) * 100, 2)
         print(f'{percentage_of_matched_tests} % des tests fonctionnent')
-    return jeux_de_parametres
+    return parameters_sets
 
-def S2_RACCParameters(majorClause: str):
+
+def S2_RACCParameters(majorClause: str, verbose=False):
     """
     Cette fonction test chaque combinaisons de paramètres d'entrée et test si le jeu de paramètre défini un
     jeu de test pour le critère RACC, i.e le changent de la clause majeure entraine un changement du prédicat,
@@ -140,57 +172,56 @@ def S2_RACCParameters(majorClause: str):
     assert majorClause in ['P', 'T2', 'T3']
     if verbose:
         print(f"Test du critère RACC pour le prédicat S2, avec la clause majeure {majorClause}")
-    jeux_de_parametres = []
-    for P in [True, False]:
-        for T2 in [True, False]:
-            for T3 in [True, False]:
-                dict = {"P": P, "T2": T2, "T3": T3}
-                dict[majorClause] = True  # On test si la clause majeure est vraie
-                test1 = S2(dict['P'], dict['T2'], dict['T3'])
-                dict[majorClause] = False  # On test si la clause majeure est fausse
-                test2 = S2(dict['P'], dict['T2'], dict['T3'])
-                if not test1 == test2:  # On vérifie que le prédicat a été une fois vrai et une fois faux
-                    dict.pop(majorClause)  # On enlève la clause majeure du jeu de paramètres
-                    jeux_de_parametres.append(dict)
+    parameters_sets = []
+    index = S2_clause_to_index(majorClause)
+    for test in S2_tests():
+        test[index] = True
+        test1 = S2(*test)
+        test[index] = False
+        test2 = S2(*test)
+        if test1 != test2:  # On vérifie que le prédicat a été une fois vrai et une fois faux
+            test.pop(index)  # On retire la clause majeure du jeu de paramètre
+            if test not in parameters_sets:  # On vérifie également que le test n'a pas été déjà effectué, car sinon on aurait chaque test en double.
+                parameters_sets.append(test)
     if verbose:
-        percentage_of_matched_tests = round(len(jeux_de_parametres)/(2**6)*100, 2)
+        percentage_of_matched_tests = round(len(parameters_sets) / (2 ** 6) * 100, 2)
         print(f'{percentage_of_matched_tests} % des tests fonctionnent')
-    return jeux_de_parametres
+    return parameters_sets
 
-def S2_RICCParameters(majorClause: str):
+
+def S2_RICCParameters(majorClause: str, verbose=False):
     """
     Cette fonction test chaque combinaisons de paramètres d'entrée et test si le jeu de paramètre défini un
-    jeu de test pour le critère RACC, i.e le changent de la clause majeure entraine un changement du prédicat,
+    jeu de test pour le critère RICC, i.e le changent de la clause majeure n'entraine pas un changement du prédicat,
     avec les mêmes clauses mineures
     :param majorClause: La clause majeure
     :return: Les jeux de paramètres dans un tableau
     """
-    assert majorClause in ['P', 'T2', 'T3']
     if verbose:
         print(f"Test du critère RICC pour le prédicat S2, avec la clause majeure {majorClause}")
-    jeux_de_parametres = []
-    for P in [True, False]:
-        for T2 in [True, False]:
-            for T3 in [True, False]:
-                dict = {"P": P, "T2": T2, "T3": T3}
-                dict[majorClause] = True  # On test si la clause majeure est vraie
-                test1 = S2(dict['P'], dict['T2'], dict['T3'])
-                dict[majorClause] = False  # On test si la clause majeure est fausse
-                test2 = S2(dict['P'], dict['T2'], dict['T3'])
-                if test1 == test2:  # On vérifie que le prédicat a été 2 fois identique
-                    dict.pop(majorClause)  # On enlève la clause majeure du jeu de paramètres
-                    jeux_de_parametres.append(dict)
+    parameters_sets = []
+    index = S2_clause_to_index(majorClause)
+    for test in S2_tests():
+        test[index] = True
+        test1 = S2(*test)
+        test[index] = False
+        test2 = S2(*test)
+        if test1 == test2:  # On vérifie que le prédicat n'a pas changé
+            test.pop(index)  # On retire la clause majeure du jeu de paramètre
+            if test not in parameters_sets:  # On vérifie également que le test n'a pas été déjà effectué, car sinon on aurait chaque test en double.
+                parameters_sets.append(test)
     if verbose:
-        percentage_of_matched_tests = round(len(jeux_de_parametres) / (2 ** 6) * 100, 2)
+        percentage_of_matched_tests = round(len(parameters_sets) / (2 ** 6) * 100, 2)
         print(f'{percentage_of_matched_tests} % des tests fonctionnent')
-    return jeux_de_parametres
+    return parameters_sets
+
 
 
 #######################################################
 ##          Génération du jeu de test                ##
 ##          pour le critère VNS du prédicat S2       ##
 #######################################################
-def S2_VNS():
+def S2_VNS(verbose=False):
     """
     S2 est dans sa forme DNF.
     """
@@ -198,13 +229,13 @@ def S2_VNS():
         print("----------------------------------------------------------")
         print("Test du critère VNS pour le prédicat S2=P + ~T3*T2, qui est dans sa forme DNF")
     # On calcule d'abord tous les jeux de tests possibles :
-    tests = [[P, T3, T2, S2(P, T3, T2)] for P in [True, False] for T3 in [True, False] for T2 in [True, False]]
+    tests = [[P, T3, T2, S2(P, T2, T3)] for P in [True, False] for T3 in [True, False] for T2 in [True, False]]
 
     # L'implicant I1 est P, on récupère les tests PUV de cet implicant
-    tests_PUV_I1 = list(filter(lambda x: x[0] and not (not x[1] and x[2]), tests)) # On veut I1, et pas I2
+    tests_PUV_I1 = list(filter(lambda x: x[0] and not (not x[1] and x[2]), tests))  # On veut I1, et pas I2
 
     # L'implicant I2 est ~T3 * T2, on récupère les tests PUV de cet implicant
-    tests_PUV_I2 = list(filter(lambda x: (not x[1] and x[2]) and not x[0], tests)) # On veut I2, et pas I1
+    tests_PUV_I2 = list(filter(lambda x: (not x[1] and x[2]) and not x[0], tests))  # On veut I2, et pas I1
 
     # P est seul dans son implicat, donc il ne peut pas avoir de PPF pour la clause P
     tests_PPF_P = []
@@ -223,7 +254,7 @@ def S2_VNS():
         if test in tests_needed:
             used = True
     if used is False:
-        tests_needed.append(tests_PUV_I1[0]) # Si on n'a pas de test correspondant, on ajoute le premier
+        tests_needed.append(tests_PUV_I1[0])  # Si on n'a pas de test correspondant, on ajoute le premier
 
     if verbose:
         print(f'{len(tests_needed)} tests utilisés :')
